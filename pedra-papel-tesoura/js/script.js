5a6a772402fe76cac00dbed3ea523789375ccc05
Julia Mangabeira <JuliaAmaralSantos@outlook.com>
2023-08-18 22:47:07 -0300

    funcao para determinar a jogada feito pelo computador, funcao para jogar com parametro da jogada do jogador quando ele clica no botao.

--- pedra-papel-tesoura/js/script.js
+++ pedra-papel-tesoura/js/script.js
@@ -1,15 +1,62 @@
+
+
 function jogadaComputador(){
 
   let numAleatorio = Math.random();
-
-  let jogadaPC = '';
-  
-  if(numAleatorio >=0 && numAleatorio < 1/3){
-    return jogadaPC = 'rock';
-  } else if(numAleatorio >= 1/3 && numAleatorio < 2/3){
-    return jogadaPC = 'paper';
+  if (numAleatorio >=0 && numAleatorio < 1/3){
+    jogadaPC = 'pedra';
+  } else if (numAleatorio >= 1/3 && numAleatorio < 2/3){
+    jogadaPC = 'papel';
   } else {
-    return jogadaPC = 'scissors';
+    jogadaPC = 'tesoura';
   }
+  return jogadaPC;
 };
+
+let placar = {
+  
+}
+
+function jogo(jogadaJogador){
+  const jogadaPC = jogadaComputador();
+  let resultado = {
+    ganhou: 'Você ganhou.',
+    perdeu: 'Você perdeu.',
+    empate: 'Empate.',
+  };
+
+  if (jogadaJogador === 'pedra'){
+    if (jogadaPC === 'papel'){
+      resultado = resultado.perdeu;
+    } else if (jogadaPC === 'tesoura'){
+      resultado = resultado.ganhou;
+    } else{
+      resultado = resultado.empate;
+    };
+};
+
+  if (jogadaJogador === 'papel'){
+    if(jogadaPC === 'pedra'){
+      resultado = resultado.ganhou;
+    } else if (jogadaPC === 'tesoura'){
+      resultado = resultado.perdeu;
+    } else{
+      resultado = resultado.empate;
+    };
+  };
+
+  if (jogadaJogador === 'tesoura'){
+    if (jogadaComputador === 'papel'){
+      resultado = resultado.ganhou;
+    } else if (jogadaComputador === 'pedra'){
+      resultado = resultado.perdeu;
+    } else{
+      resultado = resultado.empate;
+    }
+    
+  }
+console.log(`Voce escolheu: ${jogadaJogador} - Computador escolheu: ${jogadaPC}. Resultado: ${resultado}`);
+
+}
+
 
